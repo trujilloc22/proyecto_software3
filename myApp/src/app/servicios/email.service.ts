4776e5aaa31f22fd399e022d499e10ae8021e3b7
trujilloc22 <detrujilloc@uqvirtual.edu.co>
2019-06-18 02:07:06 -0500

    Update email.service.ts
    se agrega el metodo que permite enviar un email a la empresa jobymultiservicios del servicio solicitado por el cliente

--- myApp/src/app/servicios/email.service.ts
+++ myApp/src/app/servicios/email.service.ts
@@ -1,9 +1,42 @@
+/**
+ * Me permite tener centralizado el servicio de email, tambien me permite la reutilizacion de codigo.
+ */
+
 import { Injectable } from '@angular/core';
+
+/**
+ * importo el modulo que me permite enviar mensajes
+ */
+import { EmailComposer } from "@ionic-native/email-composer/ngx";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  constructor(public email: EmailComposer) { }
+
+  /**
+   * Permite enviar un correo a jobymultiservicios con la informacion de quien solicita el servicio
+   * @param nombre string que reprsenta el nombre del usuario que solicita el servicio
+   * @param email string que representa el email del usuario que solicita el servicio
+   * @param telefono number que representa el telefono del usuario que solicita el servicio
+   * @param direccion strign que representa la direccion del usuario que solicita el servicio 
+   * @param servicio string que repsernta el nombre del servicio que fue solicitado
+   */
+  enviarEmail(nombre: string, email: string, telefono: number, direccion: string, fecha_servicio: Date, servicio: string) {
+    let correo = {
+      to: "jobyServicios@gmail.com",
+      cc: [],
+      bcc: [],
+      attachment: [],
+      subject: "Solicitud de servicio",
+      body: "El cliente " + nombre + "solicitu el servicio de " + servicio + ", a continuacion se presenta los datos:\n\nnomber:" + nombre + "\nEmail:" + email + "\ntelefono:" + telefono + "\ndireccion:" + direccion + "\nfecha de servicio:" + fecha_servicio + "\nnombre de servicio:" + servicio,
+      isHtml: false
+      //app: "Gmail"
+
+    }
+
+    this.email.open(correo);
+  }
 }
