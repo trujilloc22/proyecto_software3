7785c4b588f5e2dce2e1043069ad56419af5d0a0
trujilloc22 <detrujilloc@uqvirtual.edu.co>
2019-07-03 14:43:50 -0500

    Update environment.prod.ts

--- myApp/src/environments/environment.prod.ts
+++ myApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDinMLWJ_Sb1_21Q1KHNYfZpKh5s2ndXeo",
+  authDomain: "jobymultiserviciosbd.firebaseapp.com",
+  databaseURL: "https://jobymultiserviciosbd.firebaseio.com",
+  projectId: "jobymultiserviciosbd",
+  storageBucket: "jobymultiserviciosbd.appspot.com",
+  messagingSenderId: "160567826633",
+  appId: "1:160567826633:web:b291547f366bced5"
+};
